7917e0b68a823db72522cc11487c64a11251e432
H4ltor <timothee.cognard@ynov.com>
2022-04-21 17:15:01 +0200

    [Add] upload finished

--- apps/scrap-task/src/app/scrap/scrap.service.ts
+++ apps/scrap-task/src/app/scrap/scrap.service.ts
@@ -10,7 +10,8 @@ export class ScrapService {
 
 
 
-  @Cron('*/5 * * * * *')
+  @Timeout(1000)
+  //@Cron('*/5 * * * * *')
   //@Cron('30 3 * * 1,3,5')
    Scrap(): Promise<any> {
     //this.logger.log('Cron job match');
@@ -22,11 +23,12 @@ export class ScrapService {
         awayTeamName: matchElement.querySelector('.lm3_eq2').innerText,
         homeTeamScore: matchElement.querySelector('.lm3_score').innerText.split('-')[0],
         awayTeamScore: matchElement.querySelector('.lm3_score').innerText.split('-')[1],
+        date: new Date().toISOString(),
       }) ))
       //.then(matches => this.logger.log(matches));
       //.then(matches => JSON.stringify(matches))
-      .then(matches => axios.post(`localhost:3333/api/matches/`, matches))
-      .then(response => console.log(response.data))
+      .then(matches => Promise.all(matches.map(match =>axios.post(`http://localhost:3334/api/matches/`, match ))))
+      //.then(response => console.log(response.))
       .catch(error => console.log(error));
   }
       //.then(matchesStr => this.logger.log(matchesStr));

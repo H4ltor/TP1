42257775c8d48851a93d4376cf14681b18e44cd7
H4ltor <timothee.cognard@ynov.com>
2022-05-20 19:24:23 +0200

    [Add] graphql match working

--- apps/graphql/src/app/app.module.ts
+++ apps/graphql/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from "@nestjs/mongoose";
+import {EquipesModule} from "../../../rest-api/src/app/equipes/equipes.module";
+import {MatchesModule} from "../../../rest-api/src/app/matches/matches.module";
+
+const username = 'timoth';
+const password = 'root';
+const host = 'all.9qgxg.mongodb.net';
+const databaseName = 'TP1';
+const mongoDbUri = `mongodb+srv://${username}:${password}@${host}/${databaseName}?retryWrites=true&w=majority`;
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({ useFactory: () => ({  uri: 'mongodb://localhost/TP1' }) }),
-    //MongooseModule.forRoot('mongodb://localhost/TP1'),
+    MongooseModule.forRoot(mongoDbUri), EquipesModule, MatchesModule,
+
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+

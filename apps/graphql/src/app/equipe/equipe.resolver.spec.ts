f4065d98263d162f5160a9bb419dc2d909fd5f23
H4ltor <timothee.cognard@ynov.com>
2022-05-23 22:39:20 +0200

    [Add] graphql + add end to end test

--- apps/graphql/src/app/equipe/equipe.resolver.spec.ts
+++ apps/graphql/src/app/equipe/equipe.resolver.spec.ts
@@ -1,19 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EquipeResolver } from './equipe.resolver';
-import { EquipeService } from './equipe.service';
+import {MongooseModule} from "@nestjs/mongoose";
+
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {join} from "path";
+import {EquipeModule} from "./equipe.module";
+const username = 'timoth';
+const password = 'root';
+const host = 'all.9qgxg.mongodb.net';
+const databaseName = 'TP1';
+const mongoDbUri = `mongodb+srv://${username}:${password}@${host}/${databaseName}?retryWrites=true&w=majority`;
 
 describe('EquipeResolver', () => {
   let resolver: EquipeResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EquipeResolver, EquipeService],
+      imports: [
+        MongooseModule.forRoot(mongoDbUri),
+        EquipeModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        }),
+        EquipeModule,
+      ],
     }).compile();
-
     resolver = module.get<EquipeResolver>(EquipeResolver);
   });
 
+  //test findOne
   it('should be defined', () => {
-    expect(resolver).toBeDefined();
+    expect(resolver.equipe).toBeDefined();
+  });
+  //test findAll
+  it('should be defined', () => {
+    expect(resolver.equipes).toBeDefined();
+  });
+  //test createEquipe
+  it('should be defined', () => {
+    expect(resolver.createEquipe).toBeDefined();
+  });
+  //test updateEquipe
+  it('should be defined', () => {
+    expect(resolver.updateEquipe).toBeDefined();
+  });
+  //test deleteEquipe
+  it('should be defined', () => {
+    expect(resolver.deleteEquipe).toBeDefined();
   });
 });
--- apps/graphql/src/app/match/match.resolver.spec.ts
+++ apps/graphql/src/app/match/match.resolver.spec.ts
@@ -1,19 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchResolver } from './match.resolver';
-
+import {MongooseModule} from "@nestjs/mongoose";
+import {MatchModule} from "./match.module";
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {join} from "path";
+const username = 'timoth';
+const password = 'root';
+const host = 'all.9qgxg.mongodb.net';
+const databaseName = 'TP1';
+const mongoDbUri = `mongodb+srv://${username}:${password}@${host}/${databaseName}?retryWrites=true&w=majority`;
 
 describe('MatchResolver', () => {
   let resolver: MatchResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MatchResolver],
+      imports: [
+        MongooseModule.forRoot(mongoDbUri),
+        MatchModule,
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        }),
+        MatchModule,
+      ],
     }).compile();
 
     resolver = module.get<MatchResolver>(MatchResolver);
   });
 
+  //test end to end
+
+  //test findOne
   it('should be defined', () => {
-    expect(resolver).toBeDefined();
+    expect(resolver.match).toBeDefined();
+  });
+  //test findAll
+  it('should be defined', () => {
+    expect(resolver.matches).toBeDefined();
+
+  });
+  //test createMatch
+  it('should be defined', () => {
+    expect(resolver.createMatch).toBeDefined();
+  });
+  //test updateMatch
+  it('should be defined', () => {
+    expect(resolver.updateMatch).toBeDefined();
+  });
+  //test deleteMatch
+  it('should be defined', () => {
+    expect(resolver.deleteMatch).toBeDefined();
   });
 });
